3a5e8bc14714c4e763b45d8e33ecdc718228c3c3
honghan <honghan.wu@gmail.com>
2020-12-08 15:33:22 +0000

    updated with new highlights

--- vis/js/vis.js
+++ vis/js/vis.js
@@ -4,14 +4,17 @@
         qbb.inf.getDocList(function (docs){
             let s = "";
             for(let i=0; i<docs.length;i++){
-                s += "<a id='" + docs[i].replaceAll(".", "_") + "' docId='" + docs[i] + "'>" + docs[i] + "</a>";
+                s += "<tr><td><a id='" + docs[i].replaceAll(".", "_") + "' docId='" + docs[i] + "'>" + docs[i] + "</a></td></tr>";
                 if (i == 0){
                     _curDoc = docs[i];
                 }
             }
-            $('.docList').html(s);
+            $('.docList').html("<table id='docListTab' class=\"display\"><thead><tr><th>doc ids</th></tr></thead>" + s + "</table>");
             $('.docList a').click(function (){
                 getDocDetail($(this).attr('docId'));
+            });
+            $('#docListTab').DataTable({
+                "order": [[ 0, "asc" ]]
             });
             getDocDetail(_curDoc);
         });
@@ -40,8 +43,9 @@
         })
     }
 
-    function formatAnnotation(ann){
+    function formatAnnotation(ann, index){
         return {
+            "index": index,
             "term": "",
             "s": ann['start'],
             "e": ann['end'],
@@ -50,20 +54,84 @@
             "negation": ann.negation,
             "experiencer": ann.experiencer,
             "temporality": ann.temporality,
-            "hypothetical": ann.temporality === "hypothetical"
+            "hypothetical": ann.temporality,
+            "category": "umls"
         }
+    }
+
+    function formatPhenotypeAnn(ann, index){
+        return {
+            "index": index,
+            "term": "",
+            "s": ann['start'],
+            "e": ann['end'],
+            "t": ann.minor_type,
+            "label": ann.str,
+            "negation": ann.negation,
+            "experiencer": ann.experiencer,
+            "temporality": ann.temporality,
+            "hypothetical": ann.temporality,
+            "category": "customised"
+        }
+    }
+
+    /**
+     * sort and filter out overlapped annotations (keep the max length annotations)
+     * @param anns
+     * @returns {[]} return the sorted and deduplicated annotations
+     */
+    function sortAndFilterAnnotations(anns){
+        let sorted_anns = anns.sort(function(a, b){
+            return a["s"] - b["s"];
+        });
+        let overlaps = [];
+        for(let i=0;i<sorted_anns.length;i++){
+            let a = sorted_anns[i];
+            let overlap = [];
+            for(let j=i+1;j<sorted_anns.length;j++){
+                let b = sorted_anns[j];
+                if (a.e > b.s){
+                    if (overlap.length === 0)
+                        overlap.push(a);
+                    overlap.push(b);
+                }else
+                    break;
+            }
+            if (overlap.length > 0)
+                overlaps.push(overlap);
+        }
+        let anns2remove = [];
+        for(let i=0;i<overlaps.length;i++){
+            let sortedOverlap = overlaps[i].sort(function (a, b){
+                return parseInt(a.e) - parseInt(a.s) - parseInt(b.e) + parseInt(b.s);
+            });
+            for(let j=0; j< sortedOverlap.length - 1;j++){
+                anns2remove.push(sortedOverlap[j].index);
+            }
+        }
+
+        console.log("annotations to remove: ", anns2remove);
+        let newAnns = [];
+        for(let i=0;i<anns.length;i++){
+            if (!(anns2remove.includes(anns[i].index)))
+                newAnns.push(anns[i]);
+        }
+        console.log("removed: ", anns.length - newAnns.length);
+        return newAnns;
     }
 
     function highlight(ann_ret, text, snippet) {
         let idx;
         let hos = [];
-            let anns = ann_ret['annotations']
-        for (idx in anns){
-            hos.push(formatAnnotation(anns[idx]));
+        let anns = ann_ret['annotations'];
+        console.log(ann_ret);
+        for (idx in ann_ret['annotations']){
+            hos.push(formatAnnotation(anns[idx], hos.length));
         }
-        hos = hos.sort(function(a, b){
-            return a["s"] - b["s"];
-        });
+        for (idx in ann_ret['phenotypes']){
+            hos.push(formatPhenotypeAnn(ann_ret['phenotypes'][idx], hos.length));
+        }
+        hos = sortAndFilterAnnotations(hos);
 
         const moreTextLen = 20;
         let new_str = "";
@@ -73,8 +141,10 @@
                 new_str += "...";
             for (let idx in hos){
                 const ann = hos[idx];
+                const cls = [ann.category, ann.negation, ann.temporality, ann.hypothetical].join(" ");
+
                 new_str += text.substring(prev_pos, ann["s"]) +
-                    "<mark class='" + ann.negation + "'><span title='" +
+                    "<mark class='" + cls + "'><span title='" +
                     ann["t"] + "' class=\"ann\">" + ann["label"] + " | " +
                     ann["t"] + "</span>" + text.substring(ann["s"], ann["e"]) + "</mark>";
                 prev_pos = ann["e"];
